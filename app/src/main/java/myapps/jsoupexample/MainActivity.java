518986b6f9d04b3f954dca27406111c44a9bd5d0
Eddy Verde <everde@rollins.edu>
2016-11-22 01:16:06 -0500

    11/22 DB loops through 13199 and increment by 300 to get a range. Need to have JSOUP run only once and then Select like %chicken% statment

--- app/src/main/java/myapps/jsoupexample/MainActivity.java
+++ app/src/main/java/myapps/jsoupexample/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
                 url = "http://allrecipes.com/recipe/" + i + "/";
             try {
 
-                    ////Connect to the web site
+                    ////Connect to the web sites
                     Document document = Jsoup.connect(url).get();
                     String test = String.valueOf(document);
 
