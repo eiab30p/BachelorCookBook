afdcc2beea01aee91011cd1c5722d4f73daec21a
carlosrendon <crendon@rollins.edu>
2016-11-27 00:19:25 -0500

    Start of Preferences

--- app/src/main/java/myapps/jsoupexample/RecipeActivity.java
+++ app/src/main/java/myapps/jsoupexample/RecipeActivity.java
@@ -1,17 +1,20 @@
 package myapps.jsoupexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by Eduardo Verde on 11/26/2016.
  */
 
-public class RecipeActivity extends AppCompatActivity{
+public class RecipeActivity extends AppCompatActivity {
     private Recipes recipesDB;
     private SharedPreferences prefs;
     private SharedPreferences.OnSharedPreferenceChangeListener settingsListener;
@@ -24,18 +27,27 @@ public class RecipeActivity extends AppCompatActivity{
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-        prefs = PreferenceManager.getDefaultSharedPreferences( this);
-        initializeUserPreferences( );
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        initializeUserPreferences();
 
         settingsListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-            public void onSharedPreferenceChanged( SharedPreferences prefs, String key ) {
-                initializeUserPreferences( );
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                initializeUserPreferences();
             }
         };
 
-        prefs.registerOnSharedPreferenceChangeListener( settingsListener);
+        prefs.registerOnSharedPreferenceChangeListener(settingsListener);
     }
 
+    public void loadSettings( View v ) {
+        startActivity( new Intent( getApplicationContext( ),
+                SettingsActivity.class));
+    }
+
+
+    public void loadHistory(View v) {
+        startActivity(new Intent(getApplicationContext(), Recipes.class));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,7 +60,20 @@ public class RecipeActivity extends AppCompatActivity{
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-
-
+        switch (item.getItemId()) {
+            case R.id.recipe_history:
+                loadSettings(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
+
+
+
+
